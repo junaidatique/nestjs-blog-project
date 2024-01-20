9727ef07ac6c58353ea57f73b670914b6de95616
Junaid Atique <junaid@rush.app>
2024-01-20 18:15:45 +0500

    adding port for app through env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(app.get(ConfigService).get('APP_PORT') || 3000, '0.0.0.0');
 }
 bootstrap();
